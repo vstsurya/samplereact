d8df8d80c00f52330ebf90ce454371a21fa417db
Hem Kumar <hem.gogineni@enspireforenterprise.com>
2021-11-02 11:36:46 +0530

    Fetct api commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css';
+import FetchApiData from './components/FetchApiData';
+
+function App() {
+  return(
+    <FetchApiData/>
+  );
+}
+
+export default App;
---
+++ src/components/FetchApiData.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export default class FetchApiData extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false,
+            personsData: []
+        };
+    }
+
+    async getApiData() {
+        //Update url here with required query paramas
+        const result =  await fetch('https://jsonplaceholder.typicode.com/users', {
+            method : 'GET',
+            headers : new Headers( {
+                'Accept': 'application/json',
+                //'Authorization': 'Bearer ',
+                'Content-Type': 'application/json'
+            })
+        });
+        return result.json();
+    }
+
+    async componentDidMount() {
+        const apiResult = await this.getApiData();
+        this.setState({
+            loaded: true,
+            personsData: apiResult
+        });
+    }
+
+    render() {
+        if (this.state.loaded) {
+        return (
+            <div className='App'>
+                <h1>Data Loaded</h1>
+                <h2>No of Records</h2> : {this.state.personsData.response.noOfRecords}
+                <ul>
+                    {this.state.personsData.response.person_skills.map( personSkill => (
+                        <li key={personSkill.id}>
+                            Name : {personSkill.name} | Mail : {personSkill.email}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+        }
+        else {
+            return (
+                <div>
+                    Loading........
+                </div>
+            )
+        }
+    }
+
+}
